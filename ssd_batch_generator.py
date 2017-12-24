8adc964dc6e1bbe6e6e603388e7cb0242e8b7152
Nelson Zhao <zhaonaichuan@gmail.com>
2017-12-24 11:16:36 +0800

    fix: Fix float division in Python 2

--- ssd_batch_generator.py
+++ ssd_batch_generator.py
@@ -19,6 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+from __future__ import division
 import numpy as np
 import cv2
 import random
